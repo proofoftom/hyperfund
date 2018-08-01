8912bf009714ec8975cee80597a7629e17e0d72b
Tommy Cox <tommy.cox@protonmail.com>
2018-08-01 11:39:12 -0600

    Add ability to create proposal with title and description, currently stored in firebase

--- src/components/CreateProposal.vue
+++ src/components/CreateProposal.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="flex flex-center column">
     <h2>Create a Proposal</h2>
+    <q-field helper="Name the potential outcome to be funded.">
+      <q-input
+        type="text"
+        float-label="Title"
+        v-model="title"
+      />
+    </q-field>
     <q-field
       helper="Describe the potential outcome to be funded."
       :count="120"
@@ -8,10 +15,10 @@
       <q-input
         type="textarea"
         float-label="Proposal"
-        v-model="proposal"
+        v-model="description"
       />
     </q-field>
-    <q-btn class="q-mt-xl" label="Create Proposal" />
+    <q-btn class="q-mt-xl" label="Create Proposal" @click="createProposal()" />
   </div>
 </template>
 
@@ -19,12 +26,26 @@
 </style>
 
 <script>
+import { proposalsRef } from '../plugins/firebase';
+
 export default {
   name: 'CreateProposal',
   data() {
     return {
-      proposal: '',
+      title: '',
+      description: '',
     };
+  },
+  methods: {
+    createProposal() {
+      proposalsRef.push({
+        title: this.title,
+        description: this.description,
+        amountFunded: 0,
+        edit: false,
+      });
+      this.$router.push('/proposals');
+    },
   },
 };
 </script>
