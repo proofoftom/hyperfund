9edda90ca0acf16e54466cf81a4635b2747eae3a
Tommy Cox <tommy.cox@protonmail.com>
2018-08-01 17:23:47 -0600

    Added usage instructions to landing page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,14 @@
   <q-page class="flex flex-center column">
     <h3>HyperFund</h3>
     <h4>Quickly Fund Outcomes</h4>
+    <ol style="text-align: center">
+      <li>List your proposal and its potential outcomes.</li>
+      <li>Funders race to vote in favor of one of the potential outcomes.</li>
+      <li>
+        When a proposal's funding cap is reached,
+        the contract pays out in favor of the winning outcome.
+      </li>
+    </ol>
   </q-page>
 </template>
 
