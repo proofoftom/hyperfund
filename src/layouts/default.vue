8465f00b424a17918caca1452dedd8d473703773
Tommy Cox <tommy.cox@protonmail.com>
2018-08-02 10:30:45 -0600

    Add account and balance to header toolbar

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -17,8 +17,13 @@
         </q-btn>
 
         <q-toolbar-title>
-          <a href="/" style="text-decoration:none; color: white;">HyperFund</a>
+          <a href="/" style="text-decoration: none; color: white;">HyperFund</a>
           <div slot="subtitle">Quickly Fund Outcomes</div>
+        </q-toolbar-title>
+
+        <q-toolbar-title style="float: right; text-align: right;">
+          Balance: Ξ {{ balance }} ETH
+          <div slot="subtitle">Account: {{ account }}</div>
         </q-toolbar-title>
       </q-toolbar>
     </q-layout-header>
@@ -58,13 +63,41 @@
 </template>
 
 <script>
+import Web3 from 'web3';
+
+const web3 = new Web3(window.web3.currentProvider);
 
 export default {
   name: 'LayoutDefault',
   data() {
     return {
       leftDrawerOpen: this.$q.platform.is.desktop,
+      account: 'Please connect to MetaMask',
+      balance: 'Waiting to connect...',
     };
+  },
+  methods: {
+    getAccount() {
+      web3.eth.getAccounts().then((_accounts) => {
+        [this.account] = _accounts;
+      });
+    },
+    getBalance() {
+      web3.eth.getBalance(this.account).then((_balance) => {
+        const amountInEther = web3.utils.fromWei(_balance);
+        this.balance = amountInEther;
+      });
+    },
+  },
+  watch: {
+    // whenever question changes, this function will run
+    account() {
+      this.balance = 'Retrieving...';
+      this.getBalance();
+    },
+  },
+  mounted() {
+    this.getAccount();
   },
 };
 </script>
